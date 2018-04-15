3bb5b7a1351491861612b0b0264d69dd35d14594
kpopse7en <yur.vargas@alumnos.duoc.cl>
2018-04-15 17:13:30 -0300

    tabla

--- EJEMPLO.v1.2/Vista/BuscarContrato.xaml.cs
+++ EJEMPLO.v1.2/Vista/BuscarContrato.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,29 @@ namespace Vista
     /// </summary>
     public partial class BuscarContrato : Window
     {
+
+        DataTable tablita = new DataTable();
+
         public BuscarContrato()
         {
             InitializeComponent();
+
+            tablita.Columns.Add("Id");
+            tablita.Columns.Add("Creación");
+            tablita.Columns.Add("Termino");
+            tablita.Columns.Add("Titular");
+            tablita.Columns.Add("Plan");
+            tablita.Columns.Add("Poliza");
+            tablita.Columns.Add("Inicio Vig");
+            tablita.Columns.Add("Fin Vig");
+            tablita.Columns.Add("Vig");
+            tablita.Columns.Add("Salud");
+            tablita.Columns.Add("Prima Anual");
+            tablita.Columns.Add("Prima Mensual");
+            tablita.Columns.Add("Observaciones");
+            tbl_contrato.IsItemItsOwnContainer(tablita);
+
+
         }
     }
 }
