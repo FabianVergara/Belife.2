80c229e56f37b73c51be01da80a2143743cfb2e9
kpopse7en <yur.vargas@alumnos.duoc.cl>
2018-04-14 22:24:11 -0300

    Codigo validando ceros de fechas

--- EJEMPLO.v1.2/Vista/CrearContrato.xaml.cs
+++ EJEMPLO.v1.2/Vista/CrearContrato.xaml.cs
@@ -58,17 +58,60 @@ namespace Vista
                 DateTime fechahoy = DateTime.Now;
                 string formatoDeOro = fechahoy.ToString("YYYYMMDDHHmmSS");
                     //validar formato fecha antes de guardar
+                    String aaaa = formatoDeOro.Substring(0, 3);
                     int mes = int.Parse(formatoDeOro.Substring(4,2));
                     int dia = int.Parse(formatoDeOro.Substring(6, 2));
                     int hora = int.Parse(formatoDeOro.Substring(8, 2));
                     int minutos = int.Parse(formatoDeOro.Substring(10, 2));
                     int segundos = int.Parse(formatoDeOro.Substring(-2));
-                    if (mes < 10)
-                    {
-                      /*  String [] partes = formatoDeOro.Split();
-                        String cadena = partes[];* to be continue*/ 
-                    }
-
+                            string MM;
+                            if (mes < 10)
+                            {
+                                MM = string.Concat("0",mes);//CONCATENO MES CON EL 0
+                            }
+                            else
+                            {
+                                MM = mes.ToString();
+                            }
+                            string DD;
+                            if (dia<10)
+                            {
+                                DD = string.Concat("0", dia);
+                            }
+                            else
+                            {
+                                DD = dia.ToString();
+                            }
+                            string hh;
+                            if (hora<10)
+                            {
+                                hh =string.Concat("0",hora);
+                            }
+                            else
+                            {
+                                hh = hora.ToString();
+                            }
+                            string min;
+                            if (minutos < 10)
+                            {
+                                min = string.Concat("0",minutos);
+                            }
+                            else
+                            {
+                                min = minutos.ToString();
+                            }
+                            string seg;
+                            if (segundos < 10)
+                            {
+                                seg = string.Concat("0", segundos);
+                            }
+                            else
+                            {
+                                seg = segundos.ToString();
+                            }
+                    String fecha_lista = (string.Concat(aaaa, MM, DD, hh, min, seg));
+                    String fecha = fecha_lista.ToString("YYYYMMDDHHmmss");
+                    
                 }
                 else
                 {
